af80cee396e1d57f1139a41c7112623e49762224
Jonny Spry <j.spry@arts.ac.uk>
2022-02-28 18:27:31 +0000

    get Favourites list updated and working as an mvp

---
+++ src/Components/FavouritesList.styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Container = styled('div')`
+	display: inline-block;
+	float: left;
+	position: absolute;
+	top: 0;
+	left: 0;
+	margin-left: 20px;
+	width: 25%;
+	height: 100%;
+
+	h3 {
+		text-align: left;
+	}
+`;
+
+export const UL = styled('ul')`
+	text-align: left;
+	list-style-type: none;
+	list-style-position: inside;
+	padding-inline-start: none !important;
+`;
